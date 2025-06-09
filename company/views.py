d0ead8eec47c168fedfbde5cec9d23e76dc7e718
Shubham Nirmal <suvamniraml456@gmail.com>
2025-06-09 16:41:37 +0545

    feat: Updating the directory structure and update the index page

--- company/views.py
+++ company/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def dashboard_view(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'company/dashboard.html')
 
 def alljobs_view(request):
-    return render(request, 'all_jobs.html')
+    return render(request, 'company/all_jobs.html')
 
 def post_choice_view(request):
-    return render(request, 'post_choice.html')
+    return render(request, 'company/post_choice.html')
