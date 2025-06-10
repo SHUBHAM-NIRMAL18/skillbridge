a9b73126500621128b946288029b3c5fbfde3a20
Shubham Nirmal <suvamniraml456@gmail.com>
2025-06-10 21:19:23 +0545

    feat: Added login templates in website app

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('index/', views.home_view, name='index'),
+    path('login/', views.login_view, name='login'),
     # Add other URL patterns for the website app here as needed
 ]
 
--- website/views.py
+++ website/views.py
@@ -6,3 +6,9 @@ def home_view(request):
     Render the home page.
     """
     return render(request, 'index.html')
+
+def login_view(request):
+    """
+    Render the login page.
+    """
+    return render(request, 'accounts/login.html')
