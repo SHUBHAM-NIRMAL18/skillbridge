4df5da55ddb267b76a7789d40fa55ccf73aa04c4
Shubham Nirmal <suvamniraml456@gmail.com>
2025-09-02 18:39:57 +0545

    refactor(superadmin): update the logout redirection for superadmin

--- skillbridge/urls.py
+++ skillbridge/urls.py
@@ -1,27 +1,24 @@
-"""
-URL configuration for skillbridge project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+
+def custom_logout_view(request):
+    """Custom logout view that redirects based on user type"""
+    is_superuser = request.user.is_superuser if request.user.is_authenticated else False
+    logout(request)
+    
+    if is_superuser:
+        return redirect('/admin/login/')
+    else:
+        return redirect('index')
 
 urlpatterns = [
+    path("admin/logout/", custom_logout_view, name='admin_logout'),
     path("admin/", admin.site.urls),
     path("company/", include("company.urls", namespace="company")),
     path('company/membership/', include(('membership.urls', 'membership'), namespace='membership')),
@@ -29,7 +26,6 @@ urlpatterns = [
     path("", include("accounts.urls")),
     path("", include(("website.urls", "website"), namespace="website")),
     path("applications/", include(("applications.urls", "applications"), namespace="applications")),
-    # Add other app URLs here as needed
 ]
 
 if settings.DEBUG:
