d374676235dd6c67bced983bd9e755234f6c9f72
Shubham Nirmal <suvamniraml456@gmail.com>
2025-10-17 19:07:08 +0545

    refactor: remove the registered model in superadmin dashboard

--- candidate/admin.py
+++ candidate/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
-from .models import Profile, SocialLink, Education, Experience, Project, Certificate
+
 
 # Register your models here.
-admin.site.register(Profile)
-admin.site.register(SocialLink)
-admin.site.register(Education)
-admin.site.register(Experience)
-admin.site.register(Project)
-admin.site.register(Certificate) 
