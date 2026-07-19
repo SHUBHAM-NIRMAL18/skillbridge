846973d212795c44ad06d98b8528b57d5bc9e9f6
Shubham Nirmal <suvamniraml456@gmail.com>
2026-07-19 21:06:22 +0545

    feat: register BlogPost model in django admin

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from website.models import Event, EventRegistration
+from website.models import Event, EventRegistration, BlogPost
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
@@ -13,4 +13,14 @@ class EventRegistrationAdmin(admin.ModelAdmin):
     list_display = ('user', 'event', 'registered_at')
     list_filter = ('event', 'registered_at')
     search_fields = ('user__email', 'event__title')
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'is_verified', 'created_at', 'updated_at')
+    list_filter = ('is_verified', 'created_at')
+    search_fields = ('title', 'author__email', 'content')
+    list_editable = ('is_verified',)
+    prepopulated_fields = {'slug': ('title',)}
+
 
