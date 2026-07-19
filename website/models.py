7f6de851fb20ece41276eda97cc20703604f3d24
Shubham Nirmal <suvamniraml456@gmail.com>
2026-07-19 21:06:12 +0545

    feat: add BlogPost model and database migrations

--- website/models.py
+++ website/models.py
@@ -49,3 +49,34 @@ class EventRegistration(models.Model):
 
     def __str__(self):
         return f"{self.user.email} -> {self.event.title}"
+
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    content = RichTextField()
+    cover_image = models.ImageField(upload_to='blog_covers/', blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            from django.utils.text import slugify
+            import uuid
+            base_slug = slugify(self.title)
+            if not base_slug:
+                base_slug = "post"
+            slug = base_slug
+            while BlogPost.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{uuid.uuid4().hex[:6]}"
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
